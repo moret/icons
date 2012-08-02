135f0dc67b7ef7279bb05a4fea761f6d1542fd7a
Danilo Moret <github@moret.pro.br>
2012-08-01 22:58:35 -0300

    changed classic green cross colors

--- icons.py
+++ icons.py
@@ -4,9 +4,7 @@ from PIL import ImageDraw
 ci_size = 320
 
 
-def cross(name, fg_color):
-    ci_bg = '#000'
-    ci_fg = fg_color
+def cross(name, fg_color, bg_color='#000'):
     ci_border = ci_size / 32
     ci_ih = ci_size - (2 * ci_border)
     ci_iw = ci_ih / 3
@@ -18,15 +16,15 @@ def cross(name, fg_color):
     rect_hor = [(ci_ip0, ci_ip1), (ci_ip2, ci_ip3)]
     rect_ver = [(ci_ip1, ci_ip0), (ci_ip3, ci_ip2)]
 
-    ci = Image.new('RGB', (ci_size, ci_size), ci_bg)
+    ci = Image.new('RGB', (ci_size, ci_size), bg_color)
     cid = ImageDraw.Draw(ci)
-    cid.rectangle(rect_hor, outline=ci_fg, fill=ci_fg)
-    cid.rectangle(rect_ver, outline=ci_fg, fill=ci_fg)
+    cid.rectangle(rect_hor, outline=fg_color, fill=fg_color)
+    cid.rectangle(rect_ver, outline=fg_color, fill=fg_color)
     ci.save(name + '.png')
 
 
 def green_cross():
-    cross('cross', '#0F0')
+    cross('cross', '#2C2', '#222')
 
 
 def ambar_cross():
